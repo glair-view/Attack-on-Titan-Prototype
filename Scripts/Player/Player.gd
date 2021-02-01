cce7db5a5e35237db875f9c072a9323ebe27ffd7
Glair <78341102+glair-view@users.noreply.github.com>
2021-02-01 09:36:36 -0800

    Updating player script and scene
    Changed player to Rigid Body
    
    Implemented variable friction
    
    Changed several variables to constants
    
    Removed unnecessary variables
    
    Changed variable names to be more descriptive/acurate
    
    Renamed _tween to fov_tween
    added _friction_tween
    
    Friction now varies based on _current_speed
    
    _current_speed is now updated every frame
    
    Fixed bug where fov would only increase by a maximum 2.0

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,19 +1,23 @@
-extends KinematicBody
+extends RigidBody
 
 # movement variables
-var _move_speed : float = 9.8
+const _MOVE_FORCE : float = 9.8
+const _MAX_FRICTION : float = 10.0
 var _current_speed : float = 0.0
-var _velocity : Vector3
-var gravity : float =  20.0
 
 
+const _CHANGE_FOV_AFTER : float = 15.0
 var _mouse_sensitivity : float = 0.3
 var _time : float = 0.0
-var _change_fov_after : float = 9.8
 
+
+# start updating fov after this speed
 # grapple hook variables
+var _current_wire_length_left : float
+var _current_wire_length_right : float
 var _max_wire_length : float = 40.0
-var _reel_speed : float = 12.2
+var _reel_force : float = 12.2
+
 
 # getting child nodes
 onready var _camera_pivote : Spatial = $CameraPivot
@@ -22,7 +26,10 @@ onready var _raycast_left : RayCast = $CameraPivot/CameraBoom/Camera/RayCastLeft
 onready var _raycast_right : RayCast = $CameraPivot/CameraBoom/Camera/RayCastRight
 onready var _hook_point_left : Node = $HookPointLeft
 onready var _hook_point_right : Node = $HookPointRight
-onready var _tween : GVTween = $GVTween
+onready var _fov_tween : GVTween = $FovTween
+onready var _friction_tween : GVTween = $FrictionTween
+onready var _floor_detector : RayCast = $FloorDetecter
+
 
 # variables effected by settings
 var strike_time : float = 0.0
@@ -34,8 +41,9 @@ var dont_change_fov : bool = false
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	# sets up min max value of tween
-	_tween.set_min_max_value(_change_fov_after, 15.0)
+	# sets up min max value of tweens
+	_fov_tween.set_min_max_value(_CHANGE_FOV_AFTER, 15.0)
+	_friction_tween.set_min_max_value(15.0, 100.0)
 
 # handles aiming
 func _input(event):
@@ -46,13 +54,19 @@ func _input(event):
 
 
 
-
 # updates fov based on current speed for game feel
 func _update_fov() -> void:
-	_camera.fov = min_fov + _tween.interpolate(_current_speed)
-	_camera.fov += sin(_time) * float(_current_speed > _change_fov_after)
+	_camera.fov = min_fov + _fov_tween.interpolate(_current_speed) * fov_change
+	_camera.fov += sin(_time) * float(_current_speed > _CHANGE_FOV_AFTER)
 
+# update friction based inversely on current_speed
+func _update_friction() -> void:
+	friction = _friction_tween.interpolate(_current_speed) * _MAX_FRICTION
 
+func is_on_floor() -> bool:
+	if _floor_detector.is_colliding():
+		print("hello")
+	return _floor_detector.is_colliding()
 
 
 func _get_active_hooks() -> Array:
@@ -79,7 +93,7 @@ func _reel_hooks() -> void:
 func _handle_keys() -> void:
 	
 	# This is all just the space bar
-	if is_on_floor() and Input.is_action_just_pressed("jump_or_reel_hooks"):
+	if is_on_floor() and Input.Xis_action_just_pressed("jump_or_reel_hooks"):
 		# jump
 		pass
 	# 
@@ -93,9 +107,19 @@ func _handle_keys() -> void:
 
 
 func _process(delta):
+	# used to calculate speed
+	var current_position : Vector3 = transform.origin
+	_handle_keys()
+	
+	# update current speed
+	_current_speed = current_position.distance_to(transform.origin)
+	_update_friction()
 	if not dont_change_fov:
 		_time += delta
 		_update_fov()
+	
+	
+	
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
